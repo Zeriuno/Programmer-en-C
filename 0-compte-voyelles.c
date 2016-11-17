7d9ffe7e07a72573d3c83ce67912d59d78c9f779
Zeriuno <d.pitrolo@gmx.com>
2016-11-17 15:46:08 +0100

    Exercice de test fait, testé, validé

---
+++ 0-compte-voyelles.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  int wordlen  ;
+  int x, v = 0 ;
+  char word[50] ;
+
+  scanf("%s", word) ;
+  wordlen = strlen(word);
+  for(x = 0; x < wordlen+1; x++)
+  {
+    switch(word[x])
+    {
+      case 'a': case 'A' : case 'e': case 'E': case 'i': case 'I': case 'o': case 'O': case 'u': case 'U':
+        v++ ;
+      default: break;
+    }
+  }
+  printf("%d\n", v) ;
+  return 0          ;
+}
