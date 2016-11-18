e634425853241198c75776b607d7456c12b6567b
Zeriuno <d.pitrolo@gmx.com>
2016-11-18 11:53:35 +0100

    Oubli

--- 0-compte-voyelles.c
+++ 0-compte-voyelles.c
@@ -15,6 +15,7 @@ int main()
     {
       case 'a': case 'A' : case 'e': case 'E': case 'i': case 'I': case 'o': case 'O': case 'u': case 'U':
         v++ ;
+        break ;
       default: break;
     }
   }
